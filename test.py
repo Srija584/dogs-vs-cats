7eb3d627f244386430805e2116c913102045af41
Srija584 <Srija584@users.noreply.github.com>
2019-02-19 23:24:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,11 +11,14 @@ import glob
 from PIL import Image
 
 from tensorflow.python import keras
-from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.models import Sequential, Model
 from tensorflow.python.keras.layers import InputLayer, Input
 from tensorflow.python.keras.layers import Reshape, MaxPooling2D
-from tensorflow.python.keras.layers import Conv2D, Dense, Flatten
+from tensorflow.python.keras.layers import Conv2D, Dense, Flatten, Dropout
 from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.utils import plot_model
+from tensorflow.python.keras.layers.merge import concatenate
+from skimage import exposure
 
 
 img_dir_train = "/home/srija/tensorflow/DogCat/dogs-vs-cats/train" # Enter Directory of all images 
@@ -26,7 +29,7 @@ data_path_train = os.path.join(img_dir_train,'*g')
 data_path_test = os.path.join(img_dir_test,'*g')
 data_path_test_new = os.path.join(img_dir_test_new,'*g')
 
-num_train = 4000
+num_train = 25000
 num_test = 12500
 
 files = glob.glob(data_path_train)
@@ -34,20 +37,21 @@ count = 0
 data = []
 for f1 in sorted(files):
     count = count+1
-    if 1<= count <= 2000:
-        image = cv2.imread(f1)
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(img, (28,28))
-        #img = np.array(img)
-        img = np.reshape(img, (28*28, 1))
-        data.append(img)
-    if 12501<= count <= 14500:
-        image = cv2.imread(f1)
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(img, (28,28))
-        #img = np.array(img)
-        img = np.reshape(img, (28*28, 1))
-        data.append(img)
+    #if 1<= count <= 2000:
+    image = cv2.imread(f1)
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (28,28))
+    img = np.array(img)
+    img = np.reshape(img, (28*28, 1))
+    data.append(img)
+    #if 12501<= count <= 14500:
+        # image = cv2.imread(f1)
+        # img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # img = cv2.resize(img, (28,28))
+        # img = np.array(img)
+        # img = exposure.equalize_hist(img)
+        # img = np.reshape(img, (28*28, 1))
+        # data.append(img)
 
 train_data = data
 train_data = np.array(train_data)
@@ -62,7 +66,8 @@ for f1 in sorted(files):
     image = cv2.imread(f1)
     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     img = cv2.resize(img, (28,28))
-    #img = np.array(img)
+    img = np.array(img)
+    img = exposure.equalize_hist(img)
     img = np.reshape(img, (28*28, 1))
     data.append(img)
     
@@ -83,22 +88,6 @@ print(y.shape)
 train_labels = keras.utils.to_categorical(y, num_classes=2)
 #print(train_labels)
 print(train_labels.shape)
-
-# files = glob.glob(data_path_test_new)
-# data = []
-# for f1 in files:
-#     image = cv2.imread(f1)
-#     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     img = cv2.resize(img, (28,28))
-#     #img = np.array(img)
-#     img = np.reshape(img, (28*28, 1))
-#     data.append(img)
-# test_data_new = data
-# test_data_new = np.array(test_data_new)
-# test_data_new = np.reshape(test_data_new, (12500, 784))
-# print(test_data_new.shape)
-
-
 
 
 
@@ -135,47 +124,100 @@ img_shape_full = (img_size, img_size, 1)
 # 1 for grayscale
 num_channels = 1
 
-# Number of classes, one class for each of 10 digits.
+# Number of classes, one class for each of 2 classes.
 num_classes = 2
 
-model = Sequential()
+input_shape = (img_size*img_size, 1)
 
-#initializer = keras.initializers.glorot_normal(seed=None)
-initializer = keras.initializers.Orthogonal(gain=1.0, seed=None)
+initializer = keras.initializers.glorot_normal(seed=None)
+# initializer = keras.initializers.Orthogonal(gain=1.0, seed=None)
 
 leakyRelu = keras.layers.LeakyReLU(alpha=0.3)
 
-model.add(InputLayer(input_shape = (img_size_flat, )))
+visible = Input(shape=((img_size)*img_size, 1))
+visible1 = Reshape(img_shape_full, input_shape=input_shape)(visible)
+#visible1 = BatchNormalization()(visible1)
+# first feature extractor
+conv1 = Conv2D(64, kernel_size=7, strides = 1,  kernel_initializer=initializer,
+                activation=leakyRelu, padding = 'same')(visible1)
+pool1 = MaxPooling2D(pool_size=(3, 3), strides = 2)(conv1)
+conv1 = Conv2D(64, kernel_size=3, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+                 padding = 'same')(pool1)
+pool1 = MaxPooling2D(pool_size=(3, 3), strides = 2)(conv1)
 
-model.add(Reshape(img_shape_full))
+conv_1 = Conv2D(64, kernel_size=1, strides = 1, activation=leakyRelu, kernel_initializer=initializer,
+                 padding = 'same')(pool1)
+conv_1 = Dropout(0.2)(conv_1)
+conv_1 = MaxPooling2D(pool_size=(3, 3), strides = 1)(conv_1)
 
-model.add(Conv2D(kernel_size = 5, strides = 1, filters = 16, padding = "same",
-            kernel_initializer=initializer,activation = leakyRelu, name = "conv_layer_1"))
+conv_3 = Conv2D(64, kernel_size=3, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+                 padding = 'same')(pool1)
+conv_3 = Dropout(0.2)(conv_3)
+conv_3 = Conv2D(64, kernel_size=3, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+                 padding = 'same')(conv_3)
+conv_3 = MaxPooling2D(pool_size=(3, 3), strides = 1)(conv_3)
 
-model.add(MaxPooling2D(pool_size = 2, strides = 2))
+conv_5 = Conv2D(64, kernel_size=5, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+               padding = 'same')(pool1)
+conv_5 = Dropout(0.2)(conv_5)
+conv_5 = Conv2D(64, kernel_size=5, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+                  padding = 'same')(conv_5)
+conv_5 = MaxPooling2D(pool_size=(3, 3), strides = 1)(conv_5)
 
-model.add(Conv2D(kernel_size = 5, strides = 1, filters = 36, padding = "same",
-            kernel_initializer=initializer,activation = leakyRelu, name = "conv_layer_2"))
+conv_9 = Conv2D(64, kernel_size=9, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+                padding = 'same')(pool1)
+conv_9 = Dropout(0.2)(conv_9)
+conv_9 = Conv2D(64, kernel_size=9, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+                  padding = 'same')(conv_9)
+conv_9 = MaxPooling2D(pool_size=(3, 3), strides = 1)(conv_9)
 
-model.add(MaxPooling2D(pool_size = 2, strides = 2))
+pool_proj_1 = MaxPooling2D(pool_size=(3, 3), strides = 1)(pool1)
 
-model.add(Conv2D(kernel_size = 5, strides = 1, filters = 56, padding = "same",
-            kernel_initializer=initializer,activation = leakyRelu, name = "conv_layer_3"))
+pool_proj_1 = Conv2D(64, kernel_size=1, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+                  padding = 'same')(pool_proj_1)
 
-model.add(MaxPooling2D(pool_size = 2, strides = 2))
+merge_1 = concatenate([conv_1, conv_3, conv_5, conv_9, pool_proj_1])
 
-model.add(Conv2D(kernel_size = 5, strides = 1, filters = 80, padding = "same",
-            kernel_initializer=initializer,activation = leakyRelu, name = "conv_layer_4"))
+#___________________________2nd time__________________________________#
 
-model.add(MaxPooling2D(pool_size = 2, strides = 2))
+conv_2_1 = Conv2D(64, kernel_size=1, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+                 padding = 'same')(merge_1)
+conv_2_1 = Dropout(0.2)(conv_2_1)
+conv_2_1 = MaxPooling2D(pool_size=(3, 3), strides = 1)(conv_2_1)
 
-model.add(Flatten())
 
-model.add(Dense(128, kernel_initializer=initializer,activation = leakyRelu))
+conv_2_3 = Conv2D(92, kernel_size=3, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+                padding = 'same')(merge_1)
+conv_2_3 = Dropout(0.2)(conv_2_3)
+conv_2_3 = Conv2D(92, kernel_size=3, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+                 padding = 'same')(conv_2_3)
+conv_2_3 = MaxPooling2D(pool_size=(3, 3), strides = 1)(conv_2_3)
 
-model.add(Dense(num_classes, activation = "softmax"))
+conv_2_5 = Conv2D(92, kernel_size=5, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+                 padding = 'same')(merge_1)
+conv_2_5 = Dropout(0.2)(conv_2_5)
+conv_2_5 = Conv2D(92, kernel_size=5, strides = 1, activation=leakyRelu,kernel_initializer=initializer,
+                  padding = 'same')(conv_2_5)
+conv_2_5 = MaxPooling2D(pool_size=(3, 3), strides = 1)(conv_2_5)
+
+
+merge_3 = concatenate([conv_2_1, conv_2_3, conv_2_5])
+
+
+flat_0 = Flatten()(merge_3)
+
+output1 = Dense(128, activation=leakyRelu,kernel_initializer=initializer,
+                )(flat_0)
+output2 = Dense(num_classes, activation='softmax')(output1)
+
+model = Model(inputs=visible, outputs=output2)
+
+
 
 print(model.summary())
+
+train_data = np.expand_dims(train_data, 2)
+test_data = np.expand_dims(test_data, 2)
 
 
 from tensorflow.python.keras.optimizers import Adam
@@ -188,7 +230,7 @@ model.compile(optimizer = optimizer,
 
 model.fit(x = train_data,
             y = train_labels,
-            epochs = 110, batch_size =128)
+            epochs = 100, batch_size =128)
 
 
 result = model.evaluate(x = test_data,
@@ -201,31 +243,3 @@ for name,value in zip(model.metrics_names, result):
 
 print("{0}: {1:.2}".format(model.metrics_names[1], result[1]))
     
-# y_pred = model.predict(test_data_new)
-
-# print(y_pred)
-
-# cls_pred = np.argmax(y_pred, axis = 1)
-
-# print(cls_pred)
-
-# np.savetxt("output_400.csv", cls_pred)
-
-
-path_model = 'model.keras'
-
-tfjs.converters.save_keras_model(model, '/home/srija/tensorflow/DogCat/dogs-vs-cats/tfjs/public/models/myModel')
-model.save(path_model)
-
-# model2 = load_model(path_model)
-
-images = test_data[0:10]
-cls_true = test_labels[0:10]
-
-y_pred_1 = model.predict(x=images)
-cls_pred = np.argmax(y_pred_1, axis = 1)
-
-print(cls_true)
-print(cls_pred)
-
-print(model.summary())
